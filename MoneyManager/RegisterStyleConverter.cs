4f7b37fa6b9b7ed6350f969343a5832c6ad2e6b2
daviddcbud <daviddcbud@hotmail.com>
2014-07-12 10:27:54 -0500

    highlighting

---
+++ MoneyManager/RegisterStyleConverter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace MoneyManager
+{
+    public class RegisterStyleConverter : IMultiValueConverter 
+    {
+
+
+        public object Convert(object[] values, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            FrameworkElement targetObject = values[0] as FrameworkElement;
+            if (targetObject == null)
+            {
+                return DependencyProperty.UnsetValue;
+            }
+            var iscleared = (bool?)values[1];
+            if (!iscleared.HasValue || !iscleared.Value)
+            {
+                return targetObject.TryFindResource("clearedStyle");
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
