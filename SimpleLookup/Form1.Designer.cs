6c51ac18d9c7d03105dc0ac0269a19a7ed3dc3a8
daviddcbud <daviddcbud@hotmail.com>
2017-02-11 15:52:12 -0600

    simple

---
+++ SimpleLookup/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace SimpleLookup
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Column3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.ColumnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.txtp = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.Column3,
+            this.ColumnHeader2});
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.Location = new System.Drawing.Point(12, 53);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(620, 290);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Description";
+            this.columnHeader1.Width = 200;
+            // 
+            // Column3
+            // 
+            this.Column3.Text = "User";
+            this.Column3.Width = 120;
+            // 
+            // ColumnHeader2
+            // 
+            this.ColumnHeader2.Text = "Password";
+            this.ColumnHeader2.Width = 120;
+            // 
+            // txtp
+            // 
+            this.txtp.Location = new System.Drawing.Point(13, 13);
+            this.txtp.Name = "txtp";
+            this.txtp.PasswordChar = '*';
+            this.txtp.Size = new System.Drawing.Size(232, 20);
+            this.txtp.TabIndex = 0;
+            this.txtp.TextChanged += new System.EventHandler(this.txtp_TextChanged);
+            this.txtp.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtp_KeyDown);
+            this.txtp.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtp_KeyPress);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(644, 342);
+            this.Controls.Add(this.txtp);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "Chrissi";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader Column3;
+        private System.Windows.Forms.ColumnHeader ColumnHeader2;
+        private System.Windows.Forms.TextBox txtp;
+    }
+}
+
---
+++ SimpleLookup/Form1.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.VisualBasic.FileIO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimpleLookup
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                 
+            }
+            catch(Exception ex)
+            {
+
+            }
+        }
+
+        private void txtp_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtp_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            
+        }
+
+        private void LoadData()
+        {
+            try
+            {
+                listView1.Items.Clear();
+                var parser = new TextFieldParser("Data.txt");
+                parser.SetDelimiters(",");
+                parser.HasFieldsEnclosedInQuotes = true;
+                parser.TextFieldType = FieldType.Delimited;
+                var fields = parser.ReadFields();
+                while (fields != null)
+                {
+                    var lvw = listView1.Items.Add(fields[0]);
+                    lvw.SubItems.Add(fields[1]);
+                    lvw.SubItems.Add(EncDec.Decrypt(fields[2], txtp.Text));
+                    fields = parser.ReadFields();
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void txtp_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                LoadData();
+            }
+        }
+    }
+}
